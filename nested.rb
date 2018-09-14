c4e735fe0d10889a2c195991f85ed41c1c8081ed
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 01:32:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-programmer_hash[:alan_kay][:known_for]
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -37,6 +37,8 @@ programmer_hash[:alan_kay][:known_for]
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
