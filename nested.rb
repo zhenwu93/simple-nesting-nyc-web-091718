a68852ae456cef2bdac56a047bd34558f7de67cf
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 01:52:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,9 +109,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info => "GUI"
+     alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-
 
 end
 
