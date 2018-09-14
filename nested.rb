5a58d962fdbf5026ddce2a4756f78d98487d5d8f
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 01:34:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    return programmer_hash[:dennis_ritchie][:languages]
+    puts programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
