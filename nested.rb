1bbf076c1244cabc313ab98f0fd071b1cbf01c1a
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 01:22:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,12 +16,12 @@ def hopper
         }
      }
 
-
+puts :grace_hopper
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	alan_kay[:known_for]
+
 	programmer_hash =
  		{
         :grace_hopper => {
